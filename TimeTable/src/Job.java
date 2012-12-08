bbced4719a98e53fd0c65a819d0dc40ad19dca5a
bloodysnow <genius@tcct.zaq.ne.jp>
2012-12-08 20:58:17 +0900

    タイムテーブルクラスをBuilderパターンで作成 Signed-off-by: bloodysnow <genius@tcct.zaq.ne.jp>

---
+++ TimeTable/src/Job.java
@@ -0,0 +1,19 @@
+
+public class Job {
+	private final int start;
+	private final int end;
+	
+	public Job(int start, int end)
+	{
+		this.start = start;
+		this.end = end;
+	}
+	
+	public int getStart() {
+		return start;
+	}
+	
+	public int getEnd() {
+		return end;
+	}
+}
--- TimeTable/src/Timetable.java
+++ TimeTable/src/Timetable.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Timetable {
-
+	private final List<Job> jobs;
+	private Timetable(List<Job> jobs) {
+		this.jobs = jobs;
+	}
+	
+	public class Builder {
+		private final List<Job> jobs = new ArrayList<Job>();
+		public Builder Add(Job job) {
+			jobs.add(job);
+			return this;
+		}
+		
+		public Timetable build() {
+			return new Timetable(jobs);
+		}
+	}
 }
