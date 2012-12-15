75dcaaefd3538bd4589b4721b4ea3a322b8dc899
bloodysnow <genius@tcct.zaq.ne.jp>
2012-12-15 14:43:03 +0900

    最も辞書式順序で早い文字列生成プログラム Signed-off-by: bloodysnow <genius@tcct.zaq.ne.jp>

---
+++ MakeLessString/MakeLessString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MakeLessString
+{
+    class Solver
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string solve(string question)
+        {
+            string answer = "";
+            while (question.Length > 0)
+            {
+                question = getLesserString(question);
+                answer += question[0];
+                question = question.Substring(1);
+            }
+            return answer;
+        }
+
+        /** <summary>自身と自身を反転した文字列とのうち、小さい順序の文字列を返す</summary> */
+        private string getLesserString(string str)
+        {
+            char[] revchars = str.ToCharArray();
+            Array.Reverse(revchars);
+            string revstr = new string(revchars);
+            return (str.CompareTo(revstr) < 0) ? str : revstr;
+        }
+    }
+}
---
+++ MakeLessString/MakeLessStringTest/MakeLessStringTest.cs
@@ -0,0 +1,102 @@
+﻿using MakeLessString;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MakeLessStringTest
+{
+    /// <summary>
+    ///MakeLessStringTest のテスト クラスです。すべての
+    ///MakeLessStringTest 単体テストをここに含めます
+    ///</summary>
+    [TestClass()]
+    public class MakeLessStringTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///現在のテストの実行についての情報および機能を
+        ///提供するテスト コンテキストを取得または設定します。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 追加のテスト属性
+        // 
+        //テストを作成するときに、次の追加属性を使用することができます:
+        //
+        //クラスの最初のテストを実行する前にコードを実行するには、ClassInitialize を使用
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //クラスのすべてのテストを実行した後にコードを実行するには、ClassCleanup を使用
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //各テストを実行する前にコードを実行するには、TestInitialize を使用
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //各テストを実行した後にコードを実行するには、TestCleanup を使用
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+        /// <summary>
+        ///solve のテスト
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("MakeLessString.exe")]
+        public void solveTest()
+        {
+            Solver target = new Solver();
+            string question = "AABBCBBBAA";
+            string expected = "AAAABBBBBC";
+            string actual;
+            actual = target.solve(question);
+            Assert.AreEqual(expected, actual);
+
+            question = "ABCA";
+            expected = "AABC";
+            actual = target.solve(question);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///getLesserString のテスト
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("MakeLessString.exe")]
+        public void getLesserStringTest()
+        {
+            Solver_Accessor target = new Solver_Accessor();
+            string str = "ABAA";
+            string expected = "AABA";
+            string actual;
+            actual = target.getLesserString(str);
+            Assert.AreEqual(expected, actual);
+
+            str = "AABA";
+            expected = "AABA";
+            actual = target.getLesserString(str);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
