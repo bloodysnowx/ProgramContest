5775c3ad910b138e5d9f590b4d8af6f1d9602bdf
bloodysnowx <genius@tcct.zaq.ne.jp>
2013-01-06 17:47:01 +0900

    Add Iterator Pattern Signed-off-by: bloodysnowx <genius@tcct.zaq.ne.jp>

---
+++ IteratorPattern/src/BookShelf.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookShelf implements Aggregate {
+	private List<Book> books;
+    private int last = 0;
+    public BookShelf(int maxsize) {
+        this.books = new ArrayList<Book>(maxsize);
+    }
+    public Book getBookAt(int index) {
+        return books.get(index);
+    }
+    public void appendBook(Book book) {
+        this.books.add(book);
+        last++;
+    }
+    public int getLength() {
+        return last;
+    }
+    public Iterator iterator() {
+        return new BookShelfIterator(this);
+    }
+}
