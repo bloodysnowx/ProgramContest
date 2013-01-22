dea3376f1f5fe815bf6c362a329b92c6e626042c
bloodysnowx <genius@tcct.zaq.ne.jp>
2013-01-23 00:07:41 +0900

    pass test

--- SarumansArmy/src/SarumansArmy.js
+++ SarumansArmy/src/SarumansArmy.js
@@ -1,5 +1,27 @@
 var Solver = { 
-  solve: function(points, R) {
-    return 0;
-  }
+    solve: function(points, R) {
+        // 1. 最初の点を探す
+        // 2. 最初の点を含める最大の点を探す
+        // 3. 最大の点から円を描き含まれる点を除外し、カウントアップ
+        // 4. 1-3 を繰り返す
+        if(points.length < 1) return 0;
+
+        var startPoint = points[0];
+        var nowIndex = 0;
+
+        for(nowIndex = points.length - 1; nowIndex > 0; --nowIndex)
+        {
+            if(points[nowIndex] - startPoint <= R) break;
+        }
+        var centerPoint = points[nowIndex];
+
+        for(nowIndex = points.length - 1; nowIndex > 0; --nowIndex)
+        {
+            if(points[nowIndex] - centerPoint <= R) break;
+        }
+
+        points.splice(0, nowIndex + 1);
+
+        return this.solve(points, R) + 1;
+    }
 };
--- SarumansArmy/test/SarumansArmyTest.js
+++ SarumansArmy/test/SarumansArmyTest.js
@@ -1,20 +1,20 @@
 TestCase("SarumansArmyTest", {
-  setUp: function() {
-    this.solver = Object.create(Solver);
-  },
+    setUp: function() {
+        this.solver = Object.create(Solver);
+    },
 
-  "test ({1}, 1) should return 1": function() {
-    var points = new Array(1);
-    points[0] = 1;
-    var R = 1;
-    assertEquals(1, this.solver.solve());
-  },  
+    "test ({1}, 1) should return 1": function() {
+        var points = new Array(1);
+        points[0] = 1;
+        var R = 1;
+        assertEquals(1, this.solver.solve(points, R));
+    },  
  
-  "test ({1, 3}, 1) should return 2": function() {
-    var points = new Array(2);
-    points[0] = 1;
-    points[1] = 3;
-    var R = 1;
-    assertEquals(2, this.solver.solve());
-  }
+    "test ({1, 3}, 1) should return 2": function() {
+        var points = new Array(2);
+        points[0] = 1;
+        points[1] = 3;
+        var R = 1;
+        assertEquals(2, this.solver.solve(points, R));
+    }
 });
