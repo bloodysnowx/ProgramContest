ff43db78422b711a767b5b54cbab1d02f4b11f9b
bloodysnowx <genius@tcct.zaq.ne.jp>
2013-01-23 00:17:18 +0900

    add test

--- SarumansArmy/test/SarumansArmyTest.js
+++ SarumansArmy/test/SarumansArmyTest.js
@@ -4,17 +4,38 @@ TestCase("SarumansArmyTest", {
     },
 
     "test ({1}, 1) should return 1": function() {
-        var points = new Array(1);
-        points[0] = 1;
+        var points = [1];
         var R = 1;
         assertEquals(1, this.solver.solve(points, R));
     },  
  
     "test ({1, 3}, 1) should return 2": function() {
-        var points = new Array(2);
-        points[0] = 1;
-        points[1] = 3;
+        var points = [1, 3];
         var R = 1;
         assertEquals(2, this.solver.solve(points, R));
+    },
+
+    "test ({1, 3, 5}, 2) should return 1": function() {
+        var points = [1, 3, 5];
+        var R = 2;
+        assertEquals(1, this.solver.solve(points, R));
+    },
+
+    "test ({1, 2, 3, 4, 5, 6}, 1) should return 2": function() {
+        var points = [1, 2, 3, 4, 5, 6];
+        var R = 2;
+        assertEquals(2, this.solver.solve(points, R));
+    },
+
+    "test ({1, 2, 4, 5, 6, 7}, 1) should return 3": function() {
+        var points = [1, 2, 4, 5, 6, 7];
+        var R = 1;
+        assertEquals(3, this.solver.solve(points, R));
+    },
+
+    "test ({1, 2, 3, 4, 7, 10, 15, 16, 17, 18}, 3) should return 3": function() {
+        var points = [1, 2, 3, 4, 7, 10, 15, 16, 17, 18];
+        var R = 3;
+        assertEquals(3, this.solver.solve(points, R));
     }
 });
