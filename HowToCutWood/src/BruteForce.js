7d4cba412dbb6705dca329d30afd86b62c91ebd1
bloodysnow <genius@tcct.zaq.ne.jp>
2013-01-26 00:00:07 +0900

    add countOfUse
    Signed-off-by: bloodysnow <genius@tcct.zaq.ne.jp>

--- HowToCutWood/src/BruteForce.js
+++ HowToCutWood/src/BruteForce.js
@@ -1,4 +1,11 @@
 var BruteForceSolver = {
-    solve: function() {
+    solve: function(points) {
+	
+    },
+    countOfUse: function(length) {
+	if(length <= 1) return [[0]];
+	else if(length == 2) return [[1,1]];
+	else if(length == 3) return [[2,2,1]];
+	else if(length == 4) return [[3,3,2,1],[2,2,2,2]];
     }
 };
--- HowToCutWood/test/BruteForceSolverTest.js
+++ HowToCutWood/test/BruteForceSolverTest.js
@@ -2,7 +2,13 @@ TestCase("BruteForceSolverTest", {
     setUp: function() {
 	this.solver = Object.create(BruteForceSolver);
     },
-    "test [5, 8, 8] should return 34": function() {
+    "test countOfUse 1 should return [[0]]": function() {
+	var result = this.solver.countOfUse(1);
+	assertEquals(1, result.length);
+	assertEquals(1, result[0].length);
+	assertEquals(0, result[0][0]);
+    },
+    "test solve [5, 8, 8] should return 34": function() {
 	assertEquals(34, this.solver.solve([5, 8, 8]));
     }
 });
