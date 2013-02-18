c6bdcfe956c3b38ff5b08c5ab4eb8e63eb9cc5ce
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2013-02-18 18:20:31 +0900

    KnapsackProblemの作成開始

---
+++ KnapsackProblem/KnapsackProblem/main.mm
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  KnapsackProblem
+//
+//  Created by 岩佐 淳史 on 2013/02/18.
+//  Copyright (c) 2013年 岩佐 淳史. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <iostream>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        std::cout << "Hello, World!" << std::endl;
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
