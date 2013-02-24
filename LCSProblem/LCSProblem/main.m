aa663ef0aeca8a1a395466d63535d6e17da9a5c7
bloodysnow <genius@tcct.zaq.ne.jp>
2013-02-25 00:54:58 +0900

    LCSのひな形を作成

---
+++ LCSProblem/LCSProblem/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LCSProblem
+//
+//  Created by 岩佐 淳史 on 13/02/25.
+//  Copyright (c) 2013年 岩佐 淳史. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
