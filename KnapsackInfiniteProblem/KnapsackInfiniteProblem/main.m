458a1aa1cce821fbbe385ec96f330b704aa87bcd
bloodysnow <genius@tcct.zaq.ne.jp>
2013-03-01 22:36:44 +0900

    同じものを何回でも入れられるナップサック問題の解法を作成開始

---
+++ KnapsackInfiniteProblem/KnapsackInfiniteProblem/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  KnapsackInfiniteProblem
+//
+//  Created by 岩佐 淳史 on 13/03/01.
+//  Copyright (c) 2013年 岩佐 淳史. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
