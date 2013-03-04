61354c6ea40ff78ebfb2c25ec1123c6381472d69
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2013-03-04 18:04:34 +0900

    リファクタされた系

--- TimeTable/src/Job.java
+++ TimeTable/src/Job.java
@@ -1,25 +1,26 @@
-
+/** 1つの仕事 */
 public class Job {
 	private final int start;
 	private final int end;
-	
-	public Job(int start, int end)
-	{
+
+	public Job(int start, int end) {
 		this.start = start;
 		this.end = end;
 	}
-	
+
 	public int getStart() {
 		return start;
 	}
-	
+
 	public int getEnd() {
 		return end;
 	}
+
 	/** 含まれているかどうか */
 	public boolean isInclude(Job job) {
-		return(this.start <= job.start && job.end <= this.end);
+		return (this.start <= job.start && job.end <= this.end);
 	}
+
 	/** 重なっているかどうか */
 	public boolean isSwamped(Job job) {
 		return !(this.end <= job.start | job.end <= this.start);
--- TimeTable/src/Secretary.java
+++ TimeTable/src/Secretary.java
@@ -1,13 +1,13 @@
-
+/** タイムテーブルを管理し、最適な仕事を出力する */
 public class Secretary {
 	private final Timetable timetable;
 	private final HowToChoose choose;
-	
+
 	public Secretary(Timetable timetable, HowToChoose choose) {
 		this.timetable = timetable;
 		this.choose = choose;
 	}
-	
+
 	public Timetable getBestSuitedTimetable() {
 		return choose.calcMostSuitedTimetable(timetable);
 	}
