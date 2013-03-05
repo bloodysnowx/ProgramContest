2084888d624628253778006beb29720ea69ba7e9
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2013-03-05 17:01:39 +0900

    テストケースを追加

--- KnapsackInfiniteProblem/KnapsackInfiniteProblem/main.mm
+++ KnapsackInfiniteProblem/KnapsackInfiniteProblem/main.mm
@@ -18,10 +18,16 @@ int main(int argc, const char * argv[])
         int weights[] = { 2, 2, 4, 6, 8 };
         int values[] = { 1, 2, 4, 5, 6 };
         int STONE_COUNT = sizeof(weights) / sizeof(weights[0]);
+        
         KnapsackInfSolver* solver = new KnapsackInfSolver(max_weight, weights, values, STONE_COUNT);
         int result = solver->solve();
         solver->print();
         delete solver;
+        
+        KnapsackInfSolverZ* solverZ = new KnapsackInfSolverZ(max_weight, weights, values, STONE_COUNT);
+        result = solverZ->solve();
+        solverZ->print();
+        delete solverZ;
         
         max_weight = 100;
         int weights2[] =
@@ -51,12 +57,34 @@ int main(int argc, const char * argv[])
             9,  9,  9,  9,  9, 10, 10, 10, 10, 10,
         };
         STONE_COUNT = sizeof(weights2) / sizeof(weights2[0]);
+        
         solver = new KnapsackInfSolver(max_weight, weights2, values2, STONE_COUNT);
         result = solver->solve();
         solver->print();
         delete solver;
         
-        KnapsackInfSolverZ* solverZ = new KnapsackInfSolverZ(max_weight, weights2, values2, STONE_COUNT);
+        solverZ = new KnapsackInfSolverZ(max_weight, weights2, values2, STONE_COUNT);
+        result = solverZ->solve();
+        solverZ->print();
+        delete solverZ;
+        
+        max_weight = 20;
+        int weights3[] =
+        {
+            2,  3,  4,  5,  6,  7,  8,  9, 10, 11
+        };
+        int values3[] =
+        {
+            1,  2,  3,  4,  5,  6,  7,  8,  9, 10
+        };
+        STONE_COUNT = sizeof(weights3) / sizeof(weights3[0]);
+        
+        solver = new KnapsackInfSolver(max_weight, weights3, values3, STONE_COUNT);
+        result = solver->solve();
+        solver->print();
+        delete solver;
+        
+        solverZ = new KnapsackInfSolverZ(max_weight, weights3, values3, STONE_COUNT);
         result = solverZ->solve();
         solverZ->print();
         delete solverZ;
