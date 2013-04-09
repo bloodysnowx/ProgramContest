c23e9497bcffe6b23e31f36ed1209e5f7c8571f4
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2013-04-09 19:38:46 +0900

    分割プログラムの作成開始

---
+++ HowToPart/HowToPart/main.mm
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HowToPart
+//
+//  Created by 岩佐 淳史 on 2013/04/09.
+//  Copyright (c) 2013年 岩佐 淳史. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
