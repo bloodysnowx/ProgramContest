8bbad017eee60a5c38c0e639063591353eb9d26d
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2013-04-19 09:45:50 +0900

    ガソリン給油回数を極力少なくする給油計画をする問題の回答作成開始

---
+++ FillGas/FillGas/main.mm
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FillGas
+//
+//  Created by 岩佐 淳史 on 2013/04/19.
+//  Copyright (c) 2013年 岩佐 淳史. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
