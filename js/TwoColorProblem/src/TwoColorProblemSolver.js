f7e8c1d0101f382965c958a5a1f338ce318e395e
bloodysnow <genius@tcct.zaq.ne.jp>
2013-05-26 22:43:39 +0900

    固定文字をやめ

--- js/TwoColorProblem/src/TwoColorProblemSolver.js
+++ js/TwoColorProblem/src/TwoColorProblemSolver.js
@@ -1,14 +1,16 @@
 var TwoColorProblemSolver = {
+    color: ["blue", "red", "yellow"],
+    points: [],
     solve: function(edges) {
-        var points = new Array("blue");
         for(var i = 0; i < edges.length; ++i)
         {
-            if(points[edges[i][0]] == "blue" && points[edges[i][1]] == "blue") return false;
-            else if(points[edges[i][0]] == "red" && points[edges[i][1]] == "red") return false;
-            else if(points[edges[i][0]] == "blue") points[edges[i][1]] = "red";
-            else if(points[edges[i][0]] == "red") points[edges[i][1]] = "blue";
-            else if(points[edges[i][1]] == "blue") points[edges[i][0]] = "red";
-            else if(points[edges[i][1]] == "red") points[edges[i][0]] = "blue";
+            if(this.points[edges[i][0]] == this.color[0] && this.points[edges[i][1]] == this.color[0]) return false;
+            else if(this.points[edges[i][0]] == this.color[1] && this.points[edges[i][1]] == this.color[1]) return false;
+            else if(this.points[edges[i][0]] == this.color[0]) this.points[edges[i][1]] = this.color[1];
+            else if(this.points[edges[i][0]] == this.color[1]) this.points[edges[i][1]] = this.color[0];
+            else if(this.points[edges[i][1]] == this.color[0]) this.points[edges[i][0]] = this.color[1];
+            else if(this.points[edges[i][1]] == this.color[1]) this.points[edges[i][0]] = this.color[0];
+            else { this.points[edges[i][0]] = this.color[0]; this.points[edges[i][1]] = this.color[1];}
         }
         return true;
     }
