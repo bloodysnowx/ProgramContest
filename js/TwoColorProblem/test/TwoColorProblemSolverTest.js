0518c5932e5313e064d1c5b7edf73cffec600b4d
bloodysnow <genius@tcct.zaq.ne.jp>
2013-05-26 22:46:03 +0900

    テストを増やした

--- js/TwoColorProblem/test/TwoColorProblemSolverTest.js
+++ js/TwoColorProblem/test/TwoColorProblemSolverTest.js
@@ -7,5 +7,11 @@ TestCase("TwoColorProblemSolverTest", {
     },
     "test [[0,1],[1,2],[2,0]] should return false": function() {
         assertEquals(false, this.solver.solve([[0,1],[1,2],[2,0]]));
+    },
+    "test [[0,1],[1,2],[2,3],[3,0]] should return true": function() {
+        assertEquals(true, this.solver.solve([[0,1],[1,2],[2,3],[3,0]]));
+    },
+    "test [[0,1],[1,2],[2,3],[3,0],[0,2]] should return false": function() {
+        assertEquals(false, this.solver.solve([[0,1],[1,2],[2,3],[3,0],[0,2]]));
     }
 });
