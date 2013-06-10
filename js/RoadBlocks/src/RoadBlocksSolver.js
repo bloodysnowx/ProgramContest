16931bcaf328fb0476d85a49110af0a66b696238
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2013-06-10 15:28:56 +0900

    RoadBlocksの解法を記述(ワーシャル・フロイドを利用)

--- js/RoadBlocks/src/RoadBlocksSolver.js
+++ js/RoadBlocks/src/RoadBlocksSolver.js
@@ -1,4 +1,56 @@
 var RoadBlocksSolver = {
-    solve: function(edges) {
+    edges: [],
+    first_d: [],
+    second_d: [],
+    points: [0],
+    V: 1,
+    solve: function(V, edges) {
+        this.setUp(V, edges);
+        for(var k = 0; k < V; ++k) {
+            for(var i = 0; i < V; ++i) {
+                for(var j = 0; j < V; ++j) {
+                    if(this.first_d[i][j] > this.first_d[i][k] + this.first_d[k][j])
+                    {
+                        this.second_d[i][j] = this.first_d[i][j];
+                        this.first_d[i][j] = this.first_d[i][k] + this.first_d[k][j];
+                        this.second_d[i][j] = Math.min(this.second_d[i][j], this.first_d[i][j] + this.second_d[i][i]);
+                        this.second_d[i][j] = Math.min(this.second_d[i][j], this.first_d[i][j] + this.second_d[j][j]);
+                        this.second_d[i][j] = Math.min(this.second_d[i][j], this.first_d[i][j] + this.second_d[k][k]);
+                    }
+                    else if(this.second_d[i][j] > this.first_d[i][k] + this.first_d[k][j])
+                    {
+                        this.second_d[i][j] = this.first_d[i][k] + this.first_d[k][j];
+                    }
+                }
+            }
+        }
+        return this.second_d[0][V-1];
+    },
+    setUp: function(V, edges) {
+        this.V = V;
+        this.edges = edges;
+        this.first_d = new Array();
+        this.second_d = new Array();
+        for(var i = 0; i < V; ++i)
+        {
+            this.first_d[i] = new Array();
+            this.second_d[i] = new Array();
+            for(var j = 0; j < V; ++j)
+            {
+                this.second_d[i][j] = 32767;
+                this.first_d[i][j] = 32767;
+            }
+        }
+        for(i = 0; i < edges.length; ++i)
+        {
+            this.first_d[edges[i][0]][edges[i][1]] = this.first_d[edges[i][1]][edges[i][0]] = edges[i][2];
+        }
+        for(i = 0; i < V; ++i)
+        {
+            for(j = 0; j < V; ++j)
+            {
+                this.second_d[i][i] = Math.min(this.second_d[i][i], this.first_d[i][j] * 2);
+            }
+        }
     }
 };
--- js/RoadBlocks/test/RoadBlocksTest.js
+++ js/RoadBlocks/test/RoadBlocksTest.js
@@ -2,7 +2,11 @@ TestCase("RoadBlocksTest", {
     setUp: function() {
         this.solver = Object.create(RoadBlocksSolver);
     },
-    "test [[0,1,100],[1,2,250],[2,3,100],[1,3,200]] should return 450": function() {
-        assertEquals(450, this.solver.solve([[0,1,100],[1,2,250],[2,3,100],[1,3,200]]));
+    "test solve [[0,1,100],[1,2,250],[2,3,100],[1,3,200]] should return 450": function() {
+        assertEquals(450, this.solver.solve(4, [[0,1,100],[1,2,250],[2,3,100],[1,3,200]]));
+    },
+    "test solve [[0,1,100],[1,2,350],[2,3,100],[1,3,200]] should return 450": function() {
+        assertEquals(500, this.solver.solve(4, [[0,1,100],[1,2,350],[2,3,100],[1,3,200]]));
     }
+
 });
