3b9ad4a3d6e29180d17c9e64431703c13c5a4ee9
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2014-03-27 15:25:54 +0900

    各関数をmap, reduceを用いて1行に

--- js/FriendScore/src/FriendScore.js
+++ js/FriendScore/src/FriendScore.js
@@ -3,22 +3,11 @@ var Solver = {
        2. 間接の友達(共通の友人を持つ)かどうかチェックする
        3. カウントアップする
     */
-    solve: function(values) {
-        var max = 0;
-        for(var i = 0; i < values.length; ++i)
-            max = Math.max(max, this.countFriend(values, i));
-        return max;
-    },
-    countFriend: function(values, a) {
-        var count = 0;
-        for(var i = 0; i < values.length; ++i) count += this.isFriend(values, a, i);
-        return count;
-    },
+    solve: function(values) { return Math.max.apply(null, this.range(0, values.length).map(function(x) { return this.countFriend(values, x); }, this)); },
+    countFriend: function(values, a) { return this.sum(this.range(0, values.length).map(function(x) { return this.isFriend(values, a, x); }, this)); },
     isDirectFriend: function(values, a, b) { return values[a][b] == "Y"; },
-    isIndirectFriend: function(values, a, b) {
-        for(var i = 0; i < values.length; ++i)
-            if(a != i && b != i && values[i][a] == "Y" && values[i][b] == "Y") return true;
-        return false;
-    },
-    isFriend: function(values, a, b) { return a != b && (this.isDirectFriend(values, a, b) || this.isIndirectFriend(values, a, b)); }
+    isIndirectFriend: function(values, a, b) { return Math.max.apply(null, this.range(0, values.length).map(function(x) { return a != x && b != x && values[x][a] == "Y" && values[x][b] == "Y"; })); },
+    isFriend: function(values, a, b) { return a != b && (this.isDirectFriend(values, a, b) || this.isIndirectFriend(values, a, b)); },
+    range: function(begin, end) { return Array.apply(null, {length: end - begin}).map(Number.call, Number).map(function(x) { return x + begin; }); },
+    sum: function(values) { return values.reduce(function(x, y) { return x + y; }); }
 };
