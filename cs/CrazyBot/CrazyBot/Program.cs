9fc186a30f989d67dc8106d7e4e6441a04d550c1
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2014-03-31 19:16:01 +0900

    CrazyBotの解法を作成開始

---
+++ cs/CrazyBot/CrazyBot/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CrazyBot
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ cs/CrazyBot/CrazyBot/Properties/CrazyBotSolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CrazyBot
+{
+	public class CrazyBotSolver
+	{
+		public CrazyBotSolver ()
+		{
+		}
+
+		public Decimal solve(int n, int east, int west, int south, int north) {
+			return null;
+		}
+	}
+}
+
