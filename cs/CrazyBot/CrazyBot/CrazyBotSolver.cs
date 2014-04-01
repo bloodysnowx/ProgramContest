69c0d3be7b6e360ed18c3d4133dc1c257b7a46a2
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2014-04-01 21:21:43 +0900

    CrazyBotの解法を追加

---
+++ cs/CrazyBot/CrazyBot/CrazyBotSolver.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+/* 1. stackからデータを取得する
+ * 2. 同じ点を通ったかどうかチェックする。通っていれば通ったパターンとして破棄し、1に戻る
+ * 3. 指定された歩数歩いたかどうかチェックする。歩いていれば無事に歩けたパターンとして記録し、1に戻る
+ * 4. 4方向に歩き、歩いたデータをstackに積み、1に戻る
+ * 5. stackが空になれば、無事に歩けたパターンの確立を合算して終了する */
+
+namespace CrazyBot
+{
+	public class CrazyBotSolver
+	{
+		public Decimal solve(int n, int east, int west, int south, int north) {
+			var walkingStack = new Stack<WalkingData>();
+			var successList = new List<WalkingData>();
+			walkingStack.Push(new WalkingData());
+
+			while (walkingStack.Count > 0) {
+				var currentWalkingData = walkingStack.Pop();
+				if(currentWalkingData.isVisitedTheSamePoint()) continue;
+				if(currentWalkingData.getSteps() == n) { successList.Add (currentWalkingData); continue; }
+				walkingStack.Push((new WalkingData(currentWalkingData)).walkEast(east));
+				walkingStack.Push((new WalkingData(currentWalkingData)).walkWest(west));
+				walkingStack.Push((new WalkingData(currentWalkingData)).walkSouth(south));
+				walkingStack.Push((new WalkingData(currentWalkingData)).walkNorth(north));
+			}
+
+			return (from successWalkingData in successList select successWalkingData.Probability).Sum();
+		}
+
+		private class WalkingData {
+			private int x = 0;
+			private int y = 0;
+			public Decimal Probability { get; private set; }
+			private IList<Point> footPrints = new List<Point>();
+
+			public WalkingData(){ Probability = 1m;	}
+			public WalkingData(WalkingData origin) {
+				x = origin.x;
+				y = origin.y;
+				Probability = origin.Probability;
+				footPrints = new List<Point>(origin.footPrints);
+			}
+
+			public WalkingData walkEast(int probability) { footPrints.Add(new Point(x, y)); ++x; this.Probability *= probability / 100m; return this; }
+			public WalkingData walkWest(int probability) { footPrints.Add(new Point(x, y)); --x; this.Probability *= probability / 100m; return this; }
+			public WalkingData walkSouth(int probability) { footPrints.Add(new Point(x, y)); --y; this.Probability *= probability / 100m; return this; }
+			public WalkingData walkNorth(int probability) { footPrints.Add(new Point(x, y)); ++y; this.Probability *= probability / 100m; return this; }
+			public bool isVisitedTheSamePoint() { return footPrints.Any (p => p.X == x && p.Y == y); }
+			public int getSteps() { return footPrints.Count; }
+		}
+	}
+}
+
--- cs/CrazyBot/CrazyBot/Program.cs
+++ cs/CrazyBot/CrazyBot/Program.cs
@@ -4,9 +4,14 @@ namespace CrazyBot
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+		public static void Main(string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			var solver = new CrazyBotSolver();
+			Console.WriteLine(solver.solve(1, 25, 25, 25, 25));
+			Console.WriteLine(solver.solve(2, 25, 25, 25, 25));
+			Console.WriteLine(solver.solve(7, 50, 0, 0, 50));
+			Console.WriteLine(solver.solve(14, 50, 50, 0, 0));
+			Console.WriteLine(solver.solve(14, 25, 25, 25, 25));
 		}
 	}
 }
