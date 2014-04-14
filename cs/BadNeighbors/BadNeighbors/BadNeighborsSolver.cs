6159aa216be1b3b3230b4916f65bc3d262652979
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2014-04-14 18:31:40 +0900

    BadNeighborsの解法を実装

--- cs/BadNeighbors/BadNeighbors/BadNeighborsSolver.cs
+++ cs/BadNeighbors/BadNeighbors/BadNeighborsSolver.cs
@@ -1,16 +1,36 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+/* 1. 最初(0)を選んだ場合、最後(N)を除外する
+ * 2. 最後(N)を選んだ場合、最初(0)を除外する
+ * 3. 除外した後の数列を直線と考えて処理する(0...n)
+ * 4. 最初に最後(n)を選んだ場合の最大値を計算する -> 最後のみ(d(n))
+ * 5. 最初に最後の1つ前(n-1)を選んだ場合の最大値を計算する -> 最後の1つ前のみ(d(n-1))
+ * 6. 最初に最後の2つ前(n-2)を選んだ場合の最大値を計算する -> 最後の2つ前(d(n-2)) + 最後(d(n))
+ * 7. 最初に最後の3つ前を選んだ場合の最大値を計算する -> 最後の3つ前(d(n-3)) + Max(d(n-1), d(n))
+ * 8. ...
+ * 9. 最初に最初の1つ後を選んだ場合の最大値を計算する ->
+ * 10. 最初に最初を選んだ場合の最大値を計算する ->
+ * 11. 最初と最後のどちらかを取り除いたもののうち最大を返す */
 
 namespace BadNeighbors
 {
 	public class BadNeighborsSolver
 	{
-		public BadNeighborsSolver()
-		{
+		public int maxDonations(int[] donations) {
+			return Math.Max(maxDonationSub(donations.Skip(1).ToArray()), maxDonationSub(donations.Take(donations.Length - 1).ToArray()));
 		}
 
-		public int maxDonations(int[] donations) {
-			return 0;
+		private int maxDonationSub(int[] donations) {
+			int[] maxDonations = new int[donations.Count()];
+			maxDonations[0] = donations[0];
+			if(donations.Count() > 1) maxDonations[1] = Math.Max(donations[0], donations[1]);
+			for(int i = 2; i < donations.Count(); ++i) {
+				maxDonations[i] = Math.Max(maxDonations[i - 1], maxDonations[i - 2] + donations[i]);
+			}
+
+			return maxDonations.Last();
 		}
 	}
 }
-
--- cs/BadNeighbors/BadNeighbors/Program.cs
+++ cs/BadNeighbors/BadNeighbors/Program.cs
@@ -6,7 +6,13 @@ namespace BadNeighbors
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var solver = new BadNeighborsSolver();
+			Console.WriteLine(solver.maxDonations(new int[]{10, 3, 2, 5, 7, 8}));
+			Console.WriteLine(solver.maxDonations(new int[]{11, 15}));
+			Console.WriteLine(solver.maxDonations(new int[]{7, 7, 7, 7, 7, 7, 7}));
+			Console.WriteLine(solver.maxDonations(new int[]{1, 2, 3, 4, 5, 1, 2, 3, 4, 5}));
+			Console.WriteLine(solver.maxDonations(new int[]{94, 40, 49, 65, 21, 21, 106, 80, 92, 81, 679, 4, 61, 6, 237, 
+				12, 72, 74, 29, 95, 265, 35, 47, 1, 61, 397, 52, 72, 37, 51, 1, 81, 45, 435, 7, 36, 57, 86, 81, 72}));
 		}
 	}
 }
