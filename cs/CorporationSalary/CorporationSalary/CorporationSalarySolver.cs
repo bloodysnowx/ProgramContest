9e90d17ddc1af2a833697595a3a9644a2912865f
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2014-04-14 17:42:22 +0900

    CorporationSalaryの解法を追加

---
+++ cs/CorporationSalary/CorporationSalary/CorporationSalarySolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/* 1. 上司/部下の木構造を作成する
+ * 2. 再帰的に給料を計算していく */
+
+namespace CorporationSalary
+{
+	public class CorporationSalarySolver
+	{
+		public int solve(String[] relations)
+		{
+			IList<Staff> staffList = createRelatedStaffList(relations);
+			return (from staff in staffList select staff.getSalary()).Sum();
+		}
+
+		private IList<Staff> createRelatedStaffList(String[] relations) {
+			IList<Staff> staffList = new List<Staff>(relations.Length);
+			for(int i = 0; i < relations.Length; ++i) staffList.Add(new Staff());
+			for(int i = 0; i < staffList.Count; ++i)
+				for(int j = 0; j < relations[i].Length; ++j)
+					if(relations[i][j] == 'Y') staffList[i].addSubordinate(staffList[j]);
+
+			return staffList;
+		}
+
+		class Staff
+		{
+			private const int NOT_CALCULATED = -1;
+			private int salary = NOT_CALCULATED;
+			ISet<Staff> subordinates = new HashSet<Staff>();
+
+			public int getSalary() {
+				if(salary == NOT_CALCULATED) salary = (subordinates.Count == 0) ? 1 : (from subordinate in subordinates select subordinate.getSalary()).Sum();
+				return salary;
+			}
+
+			public void addSubordinate(Staff staff) { subordinates.Add(staff); }
+		}
+	}
+}
+
---
+++ cs/CorporationSalary/CorporationSalary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CorporationSalary
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var solver = new CorporationSalarySolver();
+			String[] relations = { "N" };
+			Console.WriteLine(solver.solve(relations));
+			relations = new string[] {"NNYN", "NNYN", "NNNN", "NYYN"};
+			Console.WriteLine(solver.solve(relations));
+			relations = new string[] {"NNNNNN", "YNYNNY", "YNNNNY", "NNNNNN", "YNYNNN", "YNNYNN" };
+			Console.WriteLine(solver.solve(relations));
+			relations = new string[] {"NYNNYN", "NNNNNN", "NNNNNN", "NNYNNN", "NNNNNN", "NNNYYN"};
+			Console.WriteLine(solver.solve(relations));
+			relations = new string[]{"NNNN", "NNNN", "NNNN", "NNNN"};
+			Console.WriteLine(solver.solve(relations));
+		}
+	}
+}
