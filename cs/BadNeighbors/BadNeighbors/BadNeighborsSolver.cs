f61592b75d5c6df72de2fcf7882f01658d9d65f5
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2014-04-14 17:42:42 +0900

    BadNeighborsの雛形を作成

---
+++ cs/BadNeighbors/BadNeighbors/BadNeighborsSolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BadNeighbors
+{
+	public class BadNeighborsSolver
+	{
+		public BadNeighborsSolver()
+		{
+		}
+
+		public int maxDonations(int[] donations) {
+			return 0;
+		}
+	}
+}
+
---
+++ cs/BadNeighbors/BadNeighbors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BadNeighbors
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
