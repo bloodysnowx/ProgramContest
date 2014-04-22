7a67c146c97af5090ea508928465154796f3a598
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2014-04-22 17:34:04 +0900

    HandShakingの解法を追加

---
+++ cs/HandShaking/HandShaking/HandShakingSolver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HandShaking
+{
+	public class HandShakingSolver
+	{
+		public long solve(int n)
+		{
+			if(n % 2 == 1) return 0;
+			if(n == 2) return 1;
+			if(n == 4) return 2;
+
+			long[] DP = new long[n + 1];
+			DP[0] = 1;
+			DP[2] = 1;
+			DP[4] = 2;
+			for(int i = 6; i <= n; i += 2) {
+				for(int j = 1; j < i; j += 2) {
+					DP[i] += DP[j - 1] * DP[i - j - 1];
+				}
+			}
+
+			return DP[n];
+		}
+	}
+}
---
+++ cs/HandShaking/HandShaking/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/* DP[n] = DP[0] * DP[n - 2] + DP[2] * DP[n - 4] + DP[4] * DP[n - 6] + ... + DP[n - 4] * DP[2] + DP[n - 2] * DP[0] */
+namespace HandShaking
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var solver = new HandShakingSolver();
+			for(int i = 2; i < 51; i += 2)
+				System.Console.WriteLine(solver.solve(i));
+		}
+	}
+}
