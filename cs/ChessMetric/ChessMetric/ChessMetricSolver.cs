387e296fc23c0b585156c7ccb07c6a3fa69e5e76
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2014-04-22 17:33:34 +0900

    ChessMetricの解法を追加

---
+++ cs/ChessMetric/ChessMetric/ChessMetricSolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ChessMetric
+{
+	public class ChessMetricSolver
+	{
+		struct Move {
+			public int x, y;
+			public Move(int x, int y) { this.x = x; this.y = y; }
+		}
+
+		long[ , , ] DP;
+		Move[] moves = new Move[]{ new Move(-2, 1), new Move(2, 1), new Move(1, 2), new Move(1, -2), 
+			new Move(2, -1), new Move(-2, -1), new Move(-1, -2), new Move(-1, 2),
+			new Move(1, 1), new Move(1, -1), new Move(-1, -1), new Move(-1, 1),
+			new Move(1, 0), new Move(0, -1), new Move(-1, 0), new Move(0, 1), 
+		};
+
+		public long solve(int size, int[] start, int[] end, int numMoves) {
+			DP = new long[size, size, numMoves + 1];
+			DP[start[0], start[1], 0] = 1;
+
+			for(int step = 0; step < numMoves; ++step) {
+				for(int x = 0; x < size; ++x) {
+					for(int y = 0; y < size; ++y) {
+						foreach(var move in moves) {
+							if(canMove(size, x, y, move))
+								DP[x + move.x, y + move.y, step + 1] += DP[x, y, step];
+						}
+					}
+				}
+			}
+			return DP[end[0], end[1], numMoves];
+		}
+
+		private bool canMove(int size, int x, int y, Move move) {
+			return canMove(size, x, move.x) && canMove(size, y, move.y);
+		}
+
+		private bool canMove(int size, int x, int move) {
+			return 0 <= x + move && x + move < size;
+		}
+	}
+}
+
---
+++ cs/ChessMetric/ChessMetric/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/* 1. 1歩で来られるパターン数を各マスに記載する(ex. 0 or 1)
+ * 2. 2歩で来られるパターン数を各マスに記載する(ex. 0, 1, 2, ..., )
+ * 3. ...
+ * 4. N歩で来られるパターン数を各マスに記載する
+ * 5. Goalのパターン数を出力する
+ * 
+ * DP[x, y, i] = DP[x - ?, y - ?, i - 1] + DP[x - ?, y - ?, i - 1] + ... + DP[x - ?, y - ?, i - 1]
+ */
+
+namespace ChessMetric
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var solver = new ChessMetricSolver();
+			System.Console.WriteLine(solver.solve(3, new int[]{ 0, 0 }, new int[]{ 1, 0 }, 1));
+			System.Console.WriteLine(solver.solve(3, new int[]{ 0, 0 }, new int[]{ 1, 2 }, 1));
+			System.Console.WriteLine(solver.solve(3, new int[]{ 0, 0 }, new int[]{ 2, 2 }, 1));
+			System.Console.WriteLine(solver.solve(3, new int[]{ 0, 0 }, new int[]{ 0, 0 }, 2));
+			System.Console.WriteLine(solver.solve(100, new int[]{ 0, 0 }, new int[]{ 0, 99 }, 50));
+		}
+	}
+}
