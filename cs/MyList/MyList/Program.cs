f3da601e7c7682ddd06ba8e236b23b8709094b0c
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2014-05-07 11:27:58 +0900

    Listデータ構造の実装開始

---
+++ cs/MyList/MyList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyList
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+
+	class MyList<T> : IEnumerable<T> {
+		#region IEnumerable implementation
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+		#region IEnumerable implementation
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+
+	}
+}
