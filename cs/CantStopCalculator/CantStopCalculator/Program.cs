23f78598978fa37b0093892b71c78d9e389dd4cd
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2014-05-20 18:27:40 +0900

    歩くべきステップ数を計算するようにした

--- cs/CantStopCalculator/CantStopCalculator/Program.cs
+++ cs/CantStopCalculator/CantStopCalculator/Program.cs
@@ -7,55 +7,41 @@ namespace CantStopCalculator
 	{
 		public static void Main(string[] args)
 		{
-			var cantStopCalculator = new CantStopCalculator();
-			/*
-			for(uint i = 2; i < 13; ++i) {
-				System.Console.WriteLine("Calc start, i = " + i);
-				System.Console.WriteLine("Combinations = " + cantStopCalculator.getCombination(i));
-			}
-
-			for(uint i = 2; i < 13; ++i) {
-				System.Console.WriteLine("Calc start, i = " + i);
-				for(uint j = 2; j < 13; ++j) {
-					System.Console.Write(cantStopCalculator.getCombination(i, j) + " ");
-				}
-				System.Console.WriteLine();
-			}
-			*/
-
 			uint[] targets = new uint[args.Length];
 			for(int i = 0; i < targets.Length; ++i) targets[i] = System.Convert.ToUInt32(args[i]);
-			var combinations = cantStopCalculator.getCombination(targets);
-			Console.WriteLine("Combinations = " + combinations);
-			double probability = (double)combinations / (double)cantStopCalculator.getAllCombination();
-			Console.WriteLine("Probability = " + probability);
-			Console.Write("Enter steps : ");
-			var line = Console.ReadLine();
-			uint steps = System.Convert.ToUInt32(line);
-			Console.WriteLine((probability * 1 - (1 - probability) * steps > 0) ? "Go!" : "Stop");
+
+			var cantStopCalculator = new CantStopCalculator(targets);
+			Console.WriteLine("Combinations = " + cantStopCalculator.getCombination());
+			Console.WriteLine("Probability = " + cantStopCalculator.getProbability());
+			Console.WriteLine("ShoudWalkSteps = " + cantStopCalculator.getShouldWalkSteps());
 			Console.ReadLine();
 		}
 	}
 
 	class CantStopCalculator {
+		uint[] targets;
+		int combination = -1;
+
+		public CantStopCalculator(uint[] targets) { this.targets = targets; }
+
 		public uint getAllCombination() { return 6 * 6 * 6 * 6; }
-		public uint getCombination(uint target) {
-			uint count = 0;
-			for(uint i = 0; i < getAllCombination(); ++i) if(isOK(getDices(i), target)) ++count;
-			return count;
+
+		public uint getCombination() {
+			if(combination >= 0) return (uint)combination;
+			combination = 0;
+			for(uint i = 0; i < getAllCombination(); ++i) foreach(var target in targets) if(isOK(getDices(i), target)) { ++combination; break; }
+			return (uint)combination;
 		}
 
-		public uint getCombination(uint targetA, uint targetB) {
-			uint count = 0;
-			for(uint i = 0; i < getAllCombination(); ++i) if(isOK(getDices(i), targetA) || isOK(getDices(i), targetB)) ++count;
-			return count;
+		public double getProbability() { return (double)getCombination() / (double)getAllCombination(); }
+
+		public uint getShouldWalkSteps() {
+			uint i = 0;
+			for(i = 0; i < 100; ++i) if(!shoudWalk(i)) break;
+			return i - 1;
 		}
 
-		public uint getCombination(uint[] targets) {
-			uint count = 0;
-			for(uint i = 0; i < getAllCombination(); ++i) foreach(var target in targets) if(isOK(getDices(i), target)) { ++count; break; }
-			return count;
-		}
+		public bool shoudWalk(uint steps) { return (getProbability() * 1 - (1 - getProbability()) * steps) > 0; }
 
 		private uint[] getDices(uint i) {
 			uint[] quotients = new uint[4];
