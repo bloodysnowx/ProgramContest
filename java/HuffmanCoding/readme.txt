f0757428a9322c9ff7826cdd5047205274ef5ee1
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2015-01-09 10:49:52 +0900

    コマンドラインからの圧縮/展開を実装

--- java/HuffmanCoding/readme.txt
+++ java/HuffmanCoding/readme.txt
@@ -4,3 +4,11 @@
 4. 二分木に沿って、文字を符号化する
 5. 先頭のdictを生成する
 6. 与えられた文字列を符号化していく
+
+gradle build && java -cp build/classes/main Main -c test.txt test.compressed
+hogehoge
+e=10,g=01,h=00,o=11;0011011000110110
+
+gradle build && java -cp build/classes/main Main -d test.compressed test.decompressed
+e=10,g=01,h=00,o=11;0011011000110110
+hogehoge
--- java/HuffmanCoding/src/main/java/Main.java
+++ java/HuffmanCoding/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.*;
 
 public class Main {
 
@@ -5,8 +6,36 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        if(args.length < 3 || !(args[0].equals("-c") || args[0].equals("-d"))) {
+            System.out.println("usage [-c | -d] source target");
+            System.out.println("-c compress");
+            System.out.println("-d decompress");
+            System.exit(1);
+        }
 
+        File sourceFile = new File(args[1]);
+        File targetFile = new File(args[2]);
+        try {
+            FileReader sourceFileReader = new FileReader(sourceFile);
+            BufferedReader bufferedReader = new BufferedReader(sourceFileReader);
+            String sourceString = bufferedReader.readLine();
+            bufferedReader.close();
+            sourceFileReader.close();
+            System.out.println(sourceString);
+
+            Coding coding = new HuffmanCoding();
+            String targetString = args[0].equals("-c") ? coding.compress(sourceString) : coding.deCompress(sourceString);
+            System.out.println(targetString);
+
+            FileWriter targetFileWriter = new FileWriter(targetFile);
+            BufferedWriter bufferedWriter = new BufferedWriter(targetFileWriter);
+            bufferedWriter.write(targetString);
+            bufferedWriter.close();
+            targetFileWriter.close();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 	}
 
 }
